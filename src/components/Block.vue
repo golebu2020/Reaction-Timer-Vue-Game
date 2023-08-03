ad49ffb199d9e273a5daf27aff93156279b47b4c
Benedict Olebu <cgolebu.dev@gmail.com>
2023-08-02 19:04:37 -0700

    initial commit

---
+++ src/components/Block.vue
@@ -0,0 +1,53 @@
+<template>
+  <div @click="stopTimer" class="block" v-if="showBlock">Click me</div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      time: null,
+      reactionTime: 0,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      console.log(this.reactionTime);
+      this.$emit("end", this.reactionTime);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.block {
+  margin-top: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50vw;
+  height: 50vh;
+  background-color: darkgreen;
+  color: whitesmoke;
+  box-shadow: 5px 10px 10px 10px rgba(0, 0, 0, 0.12);
+  border-style: solid;
+  border-radius: 10px;
+  border-width: 0;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="resultDisplay">
+    <div>Reaction Timer: {{ score }} ms</div>
+    <div class="rank">{{ rank }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) this.rank = "Ninja Fingers";
+    else if (this.score < 400) this.rank = "Rapid Reflexes";
+    else this.rank = "Snail pace...";
+  },
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;1,300&display=swap");
+.resultDisplay {
+  font-family: "Rubik", monospace;
+  margin-top: 50px;
+  background: #444;
+  color: #fff;
+  width: 40vw;
+  height: 40vh;
+  font-weight: 700;
+  font-size: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-style: none;
+  border-radius: 10px;
+  box-shadow: 2px 10px 20px 10px rgba(0, 0, 0, 0.25);
+}
+
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
